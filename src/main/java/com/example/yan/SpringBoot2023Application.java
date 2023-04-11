e3594aa1e519184cbcb5cc7c59019f5b111b30ba
yanguowei <6516@vetech.cn>
2023-04-11 21:05:53 +0800

    init

---
+++ src/main/java/com/example/yan/SpringBoot2023Application.java
@@ -0,0 +1,13 @@
+package com.example.yan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yan/SpringBoot2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
