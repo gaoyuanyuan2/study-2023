cbf9f9eea4d36dcde1a80690e0e6b05d1cfd0db8
yanguowei <6516@vetech.cn>
2023-05-16 21:18:25 +0800

    alibaba easyexcel  性能测试

--- spring-boot/src/main/java/com/yan/spring/boot/util/excel/ReadTest.java
+++ spring-boot/src/main/java/com/yan/spring/boot/util/excel/ReadTest.java
@@ -4,7 +4,6 @@ package com.yan.spring.boot.util.excel;
 import com.alibaba.excel.EasyExcel;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -17,12 +16,13 @@ public class ReadTest {
 
     @Test
     public void simpleRead() {
+        long s = System.currentTimeMillis();
         String fileName = "D:\\SY 2023年3月账单2.0.xlsx";
         TrainLianTieBillVOListener lianTieBillVOListener = new TrainLianTieBillVOListener();
         EasyExcel.read(fileName, TrainLianTieBillVO.class, lianTieBillVOListener).sheet().doRead();
         List<TrainLianTieBillVO> lianTieBillVOList = lianTieBillVOListener.getCachedDataList();
-        System.out.println(lianTieBillVOList);
-
+//        System.out.println(lianTieBillVOList);
+        System.err.println("cost:" + (System.currentTimeMillis() - s) + ",size:"+lianTieBillVOList.size());
     }
 
 
